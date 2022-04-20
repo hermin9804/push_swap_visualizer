61489dd4506226a636c6ebed909ac4db149a8789
hermin9804 <hermin9804@gmail.com>
2022-04-20 22:15:24 +0900

    fix: if 문 > 로 변경

--- run_visualizer.bash
+++ run_visualizer.bash
@@ -6,7 +6,7 @@
 #    By: mher <mher@student.42seoul.kr>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/04/20 00:29:53 by mher              #+#    #+#              #
-#    Updated: 2022/04/20 03:35:58 by mher             ###   ########.fr        #
+#    Updated: 2022/04/20 22:14:38 by mher             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -30,14 +30,6 @@ PUSH_SWAP_DIR=../push_swap
 TESTER_DIR=./push_swap_tester
 
 #==============================================================================
-# remove previous push_swap_out file
-
-if [ -e push_swap_out ]
-then
-	rm push_swap_out
-fi
-
-#==============================================================================
 # set ARG
 
 if [ $1 ]
@@ -50,6 +42,6 @@ fi
 #==============================================================================
 # run
 
-$PUSH_SWAP_DIR/push_swap $ARG >> push_swap_out
+$PUSH_SWAP_DIR/push_swap $ARG > push_swap_out
 
 ./visualizer $ARG
